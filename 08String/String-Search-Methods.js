e0fd6f75b2f7ef1a5da323370d49d0a896697d17
Mohammad Aslam <36575542+aslam420@users.noreply.github.com>
2024-03-06 12:38:56 +0530

    Create String-Search-Methods.js

---
+++ 08String/String-Search-Methods.js
@@ -0,0 +1,23 @@
+// JavaScript Search Methods :
+// 1. lastIndexOf()
+// 2. indexOf()
+// 3. match()
+// 4. endsWith()
+// 5. search()
+// 6. startsWith()
+// 7. includes()
+// 8. matchAll()
+
+// Humari main String m hm kisi particular string ko search krna chahte h like -
+// => Uss particular string ki position kya hai?
+// => Wo main string m kitni baar aayi hai?
+// => main string m wo particular string h ya nhi?
+// etc...
+// Inn sb chijo ko hm pta kr skte hai search methods k help se.
+
+// 1. indexOf() :
+// The indexOf() method returns the index of (position of) the "first" occurrence
+// of a string in a string (means 2 locate m se phle se milega wo show krega
+// aur uss locate ka pehla index de dega) :
+let str = "Please locate where 'locate' occurs!";
+console.log(str.indexOf("locate"));
