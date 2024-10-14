9ba0c693fdab7f8f73a149c5bd474f7d666a3b29
Mohammad Aslam <36575542+aslam-paasa@users.noreply.github.com>
2024-10-14 10:41:07 +0530

    JavaScript

--- AsyncJS/05-Promises.js
+++ AsyncJS/05-Promises.js
@@ -1,1 +1,40 @@
-// 41:00
+/**
+ * Promises:
+ * => Kuch code socho man mein, jo ki async code ho, mtlb ki ye side
+ *    stack m jaega aur baad m chalega main stack mai. Ab ye socho
+ *    ki humne iss code ko likha hai to iska answer kabhi aaega aur
+ *    aisa bhi ho skta hai ki answer naa aaye, promises kya hai aisa
+ *    samjho ki aap promise k andr koi v async code likh do jo mann m
+ *    aae aur promise aapko ek parchi de deta hai aur wo parchi par
+ *    by default likha hota hai waiting, parchi par 2 events hote hai
+ *    mainly, ek event ka naam hai "then" and ek event ka naam hai
+ *    "catch". Agar aapka data aa gya to parchi pe completed likh jaega
+ *    waiting ki jagha and then chalega, lekin agar data mein dikkat
+ *    aayi to catch chalega and waiting ki jagha rejected likha jaega.
+ * 
+ * => Aman raghav k paas gya aur bola ki khana lene bhej do kisi ko
+ *    Raghav ne kisi ko bhej diya and aman ko bola ye parchi le lo
+ *    jb v wo banda aa jaeha to parchi se pta chl jaega. By default
+ *    parchi pe waiting likha hai qki avi tk khana aaya nhi hai.
+ * Note: Aman : Developer
+ *       Raghav : Promise
+ * 
+ * Pattern:
+ * new Pattern(function(resolve, reject) {...})
+*/
+
+const parchi = new Promise(function(resolve, reject) {
+    /**
+     * Jaa kar ek user lao, agar wo male hai to green button nahi to
+     * red button
+    */
+    fetch(`https://randomuser.me/api/`)
+    .then(raw => raw.json)
+    .then(result => {
+        if(result.results[0].gender === 'male') {
+            resolve();
+        } else {
+            reject();
+        }
+    }) 
+})
---
+++ Local-Storage/01-index.js
@@ -0,0 +1,11 @@
+/**
+ * Local Storage:
+ * Data is not persistent means it is not stored anywhere in the browser.
+ * So, whenever we refresher we lost all the data.
+ * 
+ * Q. How to make our data persistent?
+ * => Local Storage: It is a window property(browser's feature) available
+ *    in the browser where  we can store key-value pairs to make data
+ *    persistent(permanent).
+ * => Max Storage supported is upto 10MB. 
+ * */ 
